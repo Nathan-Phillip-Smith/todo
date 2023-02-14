d94c9f939eca99fc58503db3ac95c068fa5a2082
Nathan Smith <nathansmith@Nathans-MacBook-Pro-2.local>
2023-02-14 16:03:23 -0700

    started js

---
+++ app.js
@@ -0,0 +1,105 @@
+// list creation functions
+const lists = [
+   
+]
+   let currentList = lists[0];
+
+
+
+//    list creation functions end
+
+// render function
+
+   function render() {
+    // this will hold the html that will be displayed in the sidebar
+    let listsHtml = '<ul class="list-group">';
+    // iterate through the lists to get their names
+    lists.forEach((list) => {
+      listsHtml += `<li id="${list.id}" class="list-group-item">${list.name}</li>`;
+    });
+   
+    listsHtml += '</ul>';
+    // print out the lists
+   
+    document.getElementById('lists').innerHTML = listsHtml;
+    // print out the name of the current list
+   
+    document.getElementById('current-list-name').innerText = currentList.name;
+    // iterate over the todos in the current list
+   if (currentList.todos[0]) {
+
+    let todosHtml = '<ul class="list-group-flush">';
+    currentList.todos.forEach((list) => {
+      todosHtml += `<li class="list-group-item"><input class ="check" type="checkbox">${todo.text}</li>`;
+    });
+    // print out the todos
+    document.getElementById('current-list-todos').innerHTML = todosHtml;
+   }
+   lists.forEach((list) => {
+    if (currentList.id === list.id) {
+        document.getElementById(currentList.id).classList.add("active")
+    } else {
+        document.getElementById(currentList.id).classList.remove("active")
+    }
+   })
+    
+   }
+
+//    render function end
+
+//    add / remove functions
+
+   function addTodo() {
+    const text = document.getElementById('todo-input-box').value;
+    if(text) {
+      currentList.todos.push({
+        text: text,
+        completed: false
+      })
+      render();
+    }
+   }
+
+   function addList() {
+    const text = document.getElementById('list-input-box').value;
+    if(text) {
+        let id = randomIdentifier()
+        lists.push({
+            id: id,
+            name: text,
+            todos: []
+        })
+        currentList = lists[lists.length - 1]
+        document.getElementById('list-input-box').value = ''
+    render();
+   }
+}
+
+   function removeList() {
+    render();
+   }
+
+   function removeTodo() {
+    render();
+   }
+
+   function markTodoAsCompleted() {
+    render();
+   }
+
+   function removeAllTodosCompleted() {
+    render();
+   }
+
+//    add / remove functions end
+
+function randomIdentifier() {
+    return Math.random().toString(36).slice(2)
+}
+
+      console.log(lists[lists.length-1])
+
+
+
+
+   
